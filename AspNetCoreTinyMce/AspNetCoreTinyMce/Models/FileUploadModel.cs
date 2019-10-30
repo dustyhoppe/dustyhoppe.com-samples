8020bdd4d27cfdaab3e0d0e48754780de77939ff
Dusty Hoppe <dustin.hoppe@gmail.com>
2019-10-30 05:09:12 -0400

    Post: TinyMCE image uploading with ASP.NET Core

---
+++ AspNetCoreTinyMce/AspNetCoreTinyMce/Models/FileUploadModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace AspNetCoreTinyMce.Models
+{
+    [DataContract]
+    public class FileUploadModel
+    {
+        [DataMember(Name = "fileName")]
+        public string FileName { get; set; }
+
+        [DataMember(Name = "fileBytes")]
+        public byte[] FileBytes { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# A collection of samples projects
+
+This repository contains a collection of sample projects as referenced in my [blog](https://dustyhoppe.com).
+
+## Posts
+
+- [TinyMCE image uploading with ASP.NET Core](./AspNetCoreTinyMce)
